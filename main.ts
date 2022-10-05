84d5026852b0eab6213eb4ab0df40627ef353c27
pc20211142 <115064989+pc20211142@users.noreply.github.com>
2022-10-05 14:49:33 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(1)
+    basic.showNumber(2)
+    basic.showNumber(3)
+    basic.showNumber(4)
+    basic.showNumber(5)
+    basic.showNumber(6)
+})
